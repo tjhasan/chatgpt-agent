a28a137ed1b8db9699516b83d046a4b73d2fcbb7
Matthew <47581016+mrodgers616@users.noreply.github.com>
2023-02-07 18:59:50 -0500

    deploy fixes

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 
-const data = new SlashCommandBuilder().setName("ping").setDescription("Test");
+export const data = new SlashCommandBuilder().setName("ping").setDescription("Test");
 
 export async function execute(interaction) {
   await interaction.reply("Pong");
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,16 +1,13 @@
 import { REST, Routes } from "discord.js";
 import { readdirSync } from "node:fs";
+import "dotenv/config.js";
+
+
+import {data as chatGptData} from "./commands/chatgpt.js"
 
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
-}
+commands.push(chatGptData);
 
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: "10" }).setToken(
