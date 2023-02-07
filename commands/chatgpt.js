035a0e0cc371b686eb3d6f99acab387a159bf1d1
tjhasan <tjhasan159@gmail.com>
2023-02-07 18:50:34 -0500

    added tester openai command

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -1,6 +1,22 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export const data = new SlashCommandBuilder().setName("chat").setDescription("Interact with ChatGPT in this Discord Channel");
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_TOKEN,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: "text-davinci-002",
+  prompt: "Hello world",
+});
+
+export const data = new SlashCommandBuilder()
+  .setName("chat")
+  .setDescription("Interact with ChatGPT in this Discord Channel");
+
 export async function execute(interaction) {
-  await interaction.reply("Pong");
+  await interaction.reply(completion.data.choices[0].text);
 }
