ff4bb3bf2fb6e1f7c77af78acae60703c4f63184
tjhasan <tjhasan159@gmail.com>
2023-02-07 15:29:16 -0500

    Setup simple message response

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: GatewayIntentBits.Guilds });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  message.reply("asdf");
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
