c0be6cfd735d183319ddbca55f1a3557c906fff6
tjhasan <tjhasan159@gmail.com>
2023-02-07 15:08:01 -0500

    completed simple login

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
