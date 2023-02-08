e2133b83ade8925d09ed285eb986c3295f8fcde6
tjhasan <tjhasan159@gmail.com>
2023-02-07 19:36:01 -0500

    Added input option to chatgtp command

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -8,16 +8,26 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+let prompt = null;
+
 const completion = await openai.createCompletion({
   model: "text-davinci-003",
-  prompt: "What is the meaning of life?",
+  prompt: prompt,
   max_tokens: 2056,
 });
 
 export const data = new SlashCommandBuilder()
   .setName("chat")
-  .setDescription("Interact with ChatGPT in this Discord Channel");
+  .setDescription("Interact with ChatGPT in this Discord Channel")
+  .addStringOption((option) =>
+    option
+      .setName("input")
+      .setDescription("What do you want to ask chatGPT")
+      .setRequired(true)
+  );
 
 export async function execute(interaction) {
+  prompt = interaction.options.getString("input");
+  console.log(prompt);
   await interaction.reply(completion.data.choices[0].text);
 }
