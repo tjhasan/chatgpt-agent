a08860ebef8649769e2c8bdf8c8494c3c27b0465
tjhasan <tjhasan159@gmail.com>
2023-02-10 17:30:08 -0500

    Began setup for multi-model support to waifu.js

--- commands/waifu.js
+++ commands/waifu.js
@@ -1,16 +1,36 @@
 import { SlashCommandBuilder } from "discord.js";
 import { PythonShell } from "python-shell";
 
+/*
+Need to add the following options:
+  model: which model should they run the prompt on (should be a list to choose from)
+  
+*/
 export const data = new SlashCommandBuilder()
   .setName("waifu_vision")
   .setDescription("Ask Mimir to show you the anime girl of your dreams.")
   .addStringOption((option) =>
     option
       .setName("prompt")
-      .setDescription("MUST be a comma separated list of delbooru tags.")
+      .setDescription(
+        "MUST be a comma separated list of descriptors. Use \\waitfu-help for more info."
+      )
       .setRequired(true)
   );
 
+/*
+Need to add the following options to args:
+  user input:
+    - model: Which model to use (can do)
+    - prompt: What the user wants to see (can do)
+    - negative prompt: What the user doesn't want to see (can do)
+    - seed: Random if not given. Print out seed when used.
+
+  Depends on user chosen model:
+    - sampler: Which sampler to use (can do)
+    - steps: How many steps the diffuser should take: (can do)
+    - Denoise Strength: (can do)
+*/
 let options = {
   scriptPath: "./image_download",
   args: [""],
