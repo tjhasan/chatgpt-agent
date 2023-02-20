b1cd58a8d5cdceaae72cdc60c9ab4311af4206ec
tjhasan <tjhasan159@gmail.com>
2023-02-20 15:18:25 -0500

    Moved and renamed dalle commands

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-
-export const data = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Test");
-
-export async function execute(interaction) {
-  await interaction.reply("Pong");
-}
